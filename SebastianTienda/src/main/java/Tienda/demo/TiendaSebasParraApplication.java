b2ac02238ede205ae8de8f0ec9ae6289d2402d32
Parriux <jparra11332@ufide.ac.cr>
2023-09-20 20:50:52 -0400

    Semana2

---
+++ SebastianTienda/src/main/java/Tienda/demo/TiendaSebasParraApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSebasParraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaSebasParraApplication.class, args);
+	}
+
+}
