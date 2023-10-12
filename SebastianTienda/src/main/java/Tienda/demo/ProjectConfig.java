ed779408b66ab6c9fd6c3baca26d5030b0aba979
Parriux <jparra11332@ufide.ac.cr>
2023-10-11 19:23:59 -0600

    Semana5antRecreo

--- SebastianTienda/src/main/java/Tienda/demo/ProjectConfig.java
+++ SebastianTienda/src/main/java/Tienda/demo/ProjectConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
