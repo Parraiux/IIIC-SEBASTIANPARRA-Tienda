6005598f21e962a86939a7ce6912553024102c9a
Parriux <jparra11332@ufide.ac.cr>
2023-11-15 18:33:58 -0600

    Semana9Push

--- Tienda_Parra/src/main/java/Tienda_SebasParra/demo/service/ProductoService.java
+++ Tienda_Parra/src/main/java/Tienda_SebasParra/demo/service/ProductoService.java
@@ -1,26 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package Tienda_SebasParra.demo.service;
 
 //imports de la clase
 import Tienda_SebasParra.demo.domain.Producto;
 import java.util.List;
 
+
 public interface ProductoService {
-    
-    public List <Producto> getProductos(boolean activos);
-     // Se obtiene un listado de productos en un List
-    
-   // Se obtiene un Producto, a partir del id de un producto
+    public List<Producto> getProductos(boolean activo);
+    // Se obtiene un Producto, a partir del id de un producto
     public Producto getProducto(Producto producto);
-    
-    // Se inserta un nuevo categoria si el id del producto esta vacío
-    // Se actualiza un categoria si el id del producto NO esta vacío
+    // Se inserta un nuevo producto si el id del producto esta vacío
+    // Se actualiza un producto si el id del producto NO esta vacío
     public void save(Producto producto);
-    
     // Se elimina el producto que tiene el id pasado por parámetro
     public void delete(Producto producto);
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    //Lista de productos utilizando consultas con JPQL    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
+    //Lista de productos utilizando consultas con SQL Nativo
+    public List<Producto> metodoNativo(double precioInf, double precioSup);
 }
-// Fin de la clase
